7681d38d72adff88dfdcc01d26b3bc6bb1874b8f
dlh123123 <davidlh1@uci.edu>
2022-10-16 14:26:30 -0700

    started basic attack code

---
+++ Assets/Scripts/BasicAttackScripts/BasicAttackCombo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttackCombo : MonoBehaviour
+{
+    public LayerMask _enemyLayers;
+    private IEnumerator _midAttack = null;
+    private BasicAttack _comboStart;
+
+    private void Start()
+    {
+
+    }
+}
