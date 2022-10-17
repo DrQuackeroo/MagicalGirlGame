0528f6b8d36b20db176869ad77be2a7d5551fac9
AlexGrams <alexgrams222@gmail.com>
2022-10-17 16:56:58 -0700

    Testing 3D Navigation
    Getting AI movement to work with purely 2D components is difficult and has many edge cases. Making Enemies and the ground 3D objects in disguise allows for use of 3D pathfinding, which works well in all cases. 3D colliders with an invisible material are placed over 2D sprites for the environment to generate the navmesh. Enemies are given a 3D BoxCollider and NavMeshAgent.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 
 /// <summary>
 /// Controls basic Enemy behavior
+/// 
+/// Ideas:
+/// Naive movement algorithm: move left or right, depending on the Player's position relative to the Enemy. Stop when at a certain distance to the Player.
+///     Problem: What if the Player is on a platform above/below the Enemy? Enemies will group up on their platform but won't navigate towards the Player.
+///         Enemy above Player: Compare naive direction with A* direction limited to x-axis. If they are different directions, choose the A* direction.
+///             If Enemy is above the Player and both algorithms produce the same direction, it is possible Enemy is on a slope that leads to the Player without falling.
+///             If different directions, A* will lead Enemy off their current platform and down to the Player.
+///         Enemy below Player: Harder problem. Would require jumping up to reach the Player.
+///     Problem: What if there is a pit between the Enemy and the Player? Enemies will fall into the pit.
+///         Could use ledge detection, but then the Enemy won't be able to drop down off safe platforms.
 /// </summary>
 public class Enemy : MonoBehaviour
 {
@@ -12,25 +22,67 @@ public class Enemy : MonoBehaviour
     const string playerTag = "Player";
 
     // Range at which Enemy sees the player and starts moving towards them.
-    [SerializeField] private float playerDetectionRange = 20.0f;
-    [SerializeField] private float movementSpeed = 5.0f;
+    [SerializeField] protected float _playerDetectionRange = 20.0f;
+    [SerializeField] protected float _movementSpeed = 5.0f;
 
-    private GameObject player;
+    protected GameObject _player;
+    protected Rigidbody2D _rigidbody2D;
+
+    // The direction this Enemy will move this FixedUpdate() call
+    protected Vector2 _moveDirection;
+
+    //// TODO: Testing movement by setting velocity directly
+    //protected Vector3 _currentVelocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag(playerTag);
+        _player = GameObject.FindWithTag(playerTag);
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Basic move to player
+        //if (Vector3.Distance(_player.transform.position, transform.position) <= _playerDetectionRange)
+        //{
+        //    Debug.Log("Within range of player");
+        //    //transform.Translate((_player.transform.position - transform.position).normalized * _movementSpeed * Time.deltaTime);
+        //    _rigidbody2D.MovePosition(transform.position + ((_player.transform.position - transform.position).normalized * _movementSpeed * Time.deltaTime));
+            
+        //}
+
+        //// Naive move to Player: check if Player is left or right, then move in that direction
+        //if (_player.transform.position.x < transform.position.x)
+        //{
+        //    _moveDirection = Vector2.left;
+        //}
+        //else
+        //{
+        //    _moveDirection = Vector2.right;
+        //}
+
+        //_rigidbody2D.MovePosition(_rigidbody2D.position + _moveDirection * _movementSpeed * Time.fixedDeltaTime);
+
+        //// Setting velocity directly
+        //Vector3 targetVelocity = new Vector2(_movementSpeed * _moveDirection.x * 1.0f, _rigidbody2D.velocity.y);
+        //_rigidbody2D.velocity = targetVelocity;//Vector3.SmoothDamp(_rigidbody2D.velocity, targetVelocity, ref _currentVelocity, 0.025f);
+
+        // Physics.gravity.y
+        // _rigidbody2D.add
+    }
+
     void Update()
     {
-        if (Vector3.Distance(player.transform.position, transform.position) <= playerDetectionRange)
-        {
-            Debug.Log("Within range of player");
-            //transform.Translate((player.transform.position - transform.position).normalized * movementSpeed * Time.deltaTime);
-            GetComponent<Rigidbody2D>().MovePosition(transform.position + ((player.transform.position - transform.position).normalized * movementSpeed * Time.deltaTime));
-        }
+        // TODO: Testing 3D destination setting
+        GetComponent<NavMeshAgent>().SetDestination(_player.transform.position);
+        // lock rotation
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = false;
+
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 1000.0f);
+        //GetComponent<Rigidbody>().AddForce(Vector3.left * 200.0f);
     }
 }
