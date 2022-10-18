83044394c3c15f49eac23d38b1826f8e1526fe41
TrevorVahan <tvahan@uci.edu>
2022-10-18 02:14:11 -0700

    Slight modification to the Health script
    Added a function to instantly kill the player/enemy and a function to reset the health back to the max.  Also added an adjustable boolean to easily turn on and off the Debug messages

--- Assets/Scripts/Attributes/Health.cs
+++ Assets/Scripts/Attributes/Health.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private int _maxHealth = 100;
+    [SerializeField] private bool _printToConsole = true;
     private int _currentHealth;
     private bool _isAlive;
 
@@ -15,28 +16,39 @@ public class Health : MonoBehaviour
         _isAlive = true;
     }
 
+    public void ResetHealth()
+    {
+        _currentHealth = _maxHealth;
+        _isAlive = true;
+    }
+
     public void TakeDamage(int damageTaken)
     {
         _currentHealth -= damageTaken;
-        Debug.LogFormat("{0} took {1} damage", gameObject.tag, damageTaken);
+        if (_printToConsole is true) {Debug.LogFormat("{0} took {1} damage", gameObject.tag, damageTaken);}
 
         if (_currentHealth <= 0)
         {
             _isAlive = false;
-            Debug.LogFormat("{0} died", gameObject.tag);
+            if (_printToConsole is true) {Debug.LogFormat("{0} died", gameObject.tag);}
         }
     }
 
     public void HealHealth(int healthHealed)
     {
         _currentHealth += healthHealed;
-        Debug.LogFormat("{0} healed {1} health", gameObject.tag, healthHealed);
+        if (_printToConsole is true) {Debug.LogFormat("{0} healed {1} health", gameObject.tag, healthHealed);}
 
         if (_currentHealth >= _maxHealth)
         {
             _currentHealth = _maxHealth;
-            Debug.LogFormat("{0} is at full health", gameObject.tag);
+            if (_printToConsole is true) {Debug.LogFormat("{0} is at full health", gameObject.tag);}
         }
+    }
+
+    public void Kill()
+    {
+        TakeDamage(_currentHealth);
     }
 
     public int GetHealth()
@@ -48,4 +60,5 @@ public class Health : MonoBehaviour
     {
         return _isAlive;
     }
+
 }
