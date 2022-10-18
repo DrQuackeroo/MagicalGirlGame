15b496e26b6cf648268c6e810f97128bf82bf311
Quincy <ShadowArcher141@gmail.com>
2022-10-17 17:21:09 -0700

    Making mid-air Jumps more effective
    Added a small cooldown on the jump to prevent spamming jump to use up all the jumps at once.
    
    Jumps set fall velocity to zero before jumping instead of adding the jump velocity, resulting in being able to jump while falling.
    
    However, the player is still unable to jump while standing on edges of platforms, where the GroundCheck does not reach.

--- Assets/Scripts/Input/PlayerControls.cs
+++ Assets/Scripts/Input/PlayerControls.cs
@@ -12,12 +12,13 @@ public class PlayerControls : MonoBehaviour
     [Header("Player Stats")]
     [SerializeField] private float _speedMod = 10f;
     [SerializeField] private float _jumpMod = 15f;
-
     private PlayerInputActions _playerInputActions;
     private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody;
     private float _movement;
     private float _jumpsRemaining;
+    private float _tempTime = 0f;
+    private float _jumpCD = 0.2f;
     private BasicAttackCombo _basicAttackCombo;
 
     private void Awake()
@@ -96,10 +97,13 @@ public class PlayerControls : MonoBehaviour
         {
             _jumpsRemaining = 2;
         }
-        if (_jumpsRemaining > 0)
+        if (_jumpsRemaining > 0 && Time.time > _tempTime)
         {
+            _tempTime = Time.time + _jumpCD;
             _jumpsRemaining--;
-            _rigidbody.velocity += new Vector2(0, _rigidbody.velocity.y > 0f ? _jumpMod * 0.5f : _jumpMod);
+            //_rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y > 0f ? _jumpMod * 0.5f : _jumpMod);
+            _rigidbody.velocity = new Vector2(0, _jumpMod);
+
         }
     }
 
