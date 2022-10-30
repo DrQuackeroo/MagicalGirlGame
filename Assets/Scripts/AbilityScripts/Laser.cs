d240f0e7b3bd45f6fc5dac5c3732a591993bce0a
Quincy <ShadowArcher141@gmail.com>
2022-10-30 02:01:36 -0700

    Beam, Part 3
    Made "Laser" into a prefab. Drag it into the player as a child and add it into the player's "Laser Hit Box" field for every new scene for the Beam() function to work.
    It deals damage now.
    TODO: change dmg interval, change hitbox size

--- Assets/Scripts/AbilityScripts/Laser.cs
+++ Assets/Scripts/AbilityScripts/Laser.cs
@@ -9,17 +9,29 @@
 
 //This script uses the OverlapBox that creates an invisible Box Collider that detects multiple collisions with other colliders. The OverlapBox in this case is the same size and position as the GameObject you attach it to (acting as a replacement for the BoxCollider component).
 
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class OverlapBoxExample : MonoBehaviour
+public class Laser : MonoBehaviour
 {
     public LayerMask m_LayerMask;
     private SpriteRenderer spriteRenderer;
+
+    private GameObject _owner;
+    [SerializeField] private int _damage;
+
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    public void Initialize(GameObject owner)
+    {
+        _owner = owner;
+    }
+
 
     void FixedUpdate()
     {
@@ -31,20 +43,27 @@ public class OverlapBoxExample : MonoBehaviour
     void MyCollisions()
     {
 
-
-
-        //Use the OverlapBox to detect if there are any other colliders within this box area.
-        //Use the GameObject's centre, half the size (as a radius) and rotation. This creates an invisible box around your GameObject.
+        List<Collider> uniqueEnemyColliders = new List<Collider>();
         Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, m_LayerMask);
-        int i = 0;
-        //Check when there is a new collider coming into contact with the box
-        while (i < hitColliders.Length)
+
+
+        for (int i = 0; i < hitColliders.Length; i++)
         {
-            //Output all of the collider names
             Debug.Log("Hit : " + hitColliders[i].name + i);
-            //Increase the number of Colliders in the array
-            i++;
+            if (!uniqueEnemyColliders.Contains(hitColliders[i]))
+            {
+                uniqueEnemyColliders.Add(hitColliders[i]);
+                Health enemyHealth = hitColliders[i].GetComponent<Health>();
+
+                if (enemyHealth != null)
+                    enemyHealth.TakeDamage(_damage, _owner);
+            }
+
         }
+
+
+
+
     }
 
     //Draw the Box Overlap as a gizmo to show where it currently is testing. Click the Gizmos button to see this
