d30b660d8766fd4899921bb644966d099d1312db
Alexander Grams <alexgrams222@gmail.com>
2022-11-26 17:02:14 -0800

    Boss ability bug fix
    Fixed bug where Boss would transition quickly out of animation states for certain attacks, causing it to launch multiple attacks in a short period.

--- Assets/Scripts/Enemy/States/BossAttack.cs
+++ Assets/Scripts/Enemy/States/BossAttack.cs
@@ -10,6 +10,8 @@ public class BossAttack : StateMachineBehaviour
     // The time until the attack ends. Counts down and exits state when it reaches 0.
     protected float _attackTimer;
     protected Boss _boss;
+    // Prevents AttackedEnded trigger from being set more than once during this behavior's runtime.
+    protected bool _timerHasEnded;
 
     // Trigger to exit state
     protected readonly int _hashAttackEnded = Animator.StringToHash("AttackEnded");
@@ -24,6 +26,7 @@ public class BossAttack : StateMachineBehaviour
 
         // Timer to transition out is an Animator variable set in BossActive and read here.
         animator.SetInteger(_hashAttackStateIndex, -1);
+        _timerHasEnded = false;
 
         // Hack: To prevent next attack from being started the same frame the previous attack ends, add some extra time to the timer.
         _attackTimer = animator.GetFloat(_hashCurrentAttackDuration) + 0.1f;
@@ -33,8 +36,11 @@ public class BossAttack : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         _attackTimer -= Time.deltaTime;
-        if (_attackTimer <= 0.0f)
+        if (!_timerHasEnded && _attackTimer <= 0.0f)
+        {
+            _timerHasEnded = true;
             animator.SetTrigger(_hashAttackEnded);
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
